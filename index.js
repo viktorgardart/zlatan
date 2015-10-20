f617720aa15c04259e40d796a5da35588cf2f76d
Viktor Gardart <viktor.gardart@agigen.se>
2015-10-20 18:22:13 +0200

    check if there is any arhuments

--- index.js
+++ index.js
@@ -2,7 +2,9 @@
 
 var args = process.argv.slice(2);
 
-if (args.length <= 1) {
+if (!args.length) {
+    console.log('You need to pass atleast 2 options, eg. `zlatan npm bower`');
+}else if (args.length <= 1) {
     console.log('Well just pick', args[0]);
 }else {
     var rand = args[Math.floor(Math.random() * args.length)];
