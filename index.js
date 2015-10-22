e6dd718f5874a3bde72a914e9581722f24a1b467
Viktor Gardart <viktor.gardart@agigen.se>
2015-10-22 22:33:31 +0200

    Made zlatan work in both the terminal and in nodefiles

--- index.js
+++ index.js
@@ -1,12 +1,22 @@
 #!/usr/bin/env node
 
-var args = process.argv.slice(2);
+var zlatan = function zlatan(args) {
+    if (!args.length && args.length <= 1) {
+        return 'You need to pass atleast 2 options, eg. `zlatan npm bower`';
+    }else {
+        var rand = args[Math.floor(Math.random() * args.length)];
+        return rand;
+    }
+}
 
-if (!args.length) {
-    console.log('You need to pass atleast 2 options, eg. `zlatan npm bower`');
-}else if (args.length <= 1) {
-    console.log('Well just pick', args[0]);
-}else {
-    var rand = args[Math.floor(Math.random() * args.length)];
-    console.log(rand);
+/**
+ * export zlatan
+ */
+exports = module.exports = zlatan;
+
+/**
+ * If runned from terminal excecute directly
+ */
+if (process.argv.length > 2) {
+    console.log(zlatan(process.argv.slice(2)));
 }
