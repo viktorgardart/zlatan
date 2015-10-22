34aae8a53e5d378b3f472baf25781746606a4dc2
Jonathan Nguyen <jonathan.nguyen@agigen.se>
2015-10-22 12:23:50 +0200

    Added some abzolute

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ var args = process.argv.slice(2);
 if (!args.length) {
     console.log('You need to pass atleast 2 options, eg. `zlatan npm bower`');
 }else if (args.length <= 1) {
-    console.log('Well just pick', args[0]);
+    console.log('Well just pick', args[0] + ', abzolut');
 }else {
     var rand = args[Math.floor(Math.random() * args.length)];
     console.log(rand);
