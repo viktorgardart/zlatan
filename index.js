d7d032d085038f7579212d9e0f3a6ce6924cc315
Viktor Gardart <viktor.gardart@agigen.se>
2015-10-31 00:09:05 +0100

    Made it possible to call zlatan with comma separated strings instead of passing an array of items

--- index.js
+++ index.js
@@ -1,17 +1,32 @@
 #!/usr/bin/env node
 
-var zlatan = function zlatan(args) {
-    if (!args.length || args.length <= 1) {
-        return 'You need to pass atleast 2 options, eg. `zlatan npm bower`';
-    }else {
+var Zlatan = (function() {
+    function Zlatan(args) {
+        if (!args.length || args.length <= 1) {
+            return 'You need to pass atleast 2 options, eg. `zlatan npm bower`';
+        }
+
         var rand = args[Math.floor(Math.random() * args.length)];
         return rand;
     }
-}
+
+    return Zlatan;
+}());
 
 /**
  * export zlatan
  */
+function zlatan(args) {
+    /**
+     * If the first argument is a string, get all the arguments
+     * from the 'global' arguments and store override args
+     */
+    if (typeof args === 'string') {
+        args = Array.prototype.slice.call(arguments, 0);
+    }
+    return Zlatan(args);
+}
+
 exports = module.exports = zlatan;
 
 /**
